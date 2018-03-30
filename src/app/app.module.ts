4a2ac48b2773504631b84e1a30716f58360f6fd3
AgusFran <agustinfrancioli@gmail.com>
2018-03-30 16:48:27 -0300

    FireBase And Ionic Blank app

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+
+import { HttpModule } from "@angular/http";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireModule } from "angularfire2";
+import { FirebaseProvider } from "./../providers/firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlLiEXeRcMbW-azXNTkAh_TfC659HCQKU",
+  authDomain: "dungeonsanddragons-99d7a.firebaseapp.com",
+  databaseURL: "https://dungeonsanddragons-99d7a.firebaseio.com",
+  projectId: "dungeonsanddragons-99d7a",
+  storageBucket: "dungeonsanddragons-99d7a.appspot.com",
+  messagingSenderId: "371973718487"
+};
+
+@NgModule({
+  declarations: [MyApp, HomePage],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [MyApp, HomePage],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    FirebaseProvider,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule {}
